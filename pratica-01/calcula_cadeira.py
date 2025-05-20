0a19acc15e6964d93ed5f3e26d6f78326ed5e8ef
Bruna <brunarainhalemos@gmail.com>
2025-05-20 19:45:41 -0300

    Prática 01

---
+++ pratica-01/calcula_cadeira.py
@@ -0,0 +1,15 @@
+# Esse código calcula o preço total de um produto
+
+# Var com detalhes do produto
+nome_produto = "Cadeira infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+# Var de cálculo do valor total
+preco_total = quantidade * preco_unitario
+
+# Print com o resultado final
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"Preço total:R$ {preco_total:.2f}")
---
+++ pratica-01/calcula_diferenca.py
@@ -0,0 +1,14 @@
+# Esse código lê valores inteiros inputados pelo usuário
+
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+D = int(input("Digite o valor de D: "))
+
+# Calculo da diferença
+DIFERENCA = (A * B) - (C * D)
+
+# Exibir resultado
+print("A fórmula para a diferença é: DIFERENCA = (A * B) - (C * D)")
+print(f"Substituindo os valores fornecidos: ({A} * {B}) - ({C} * {D})")
+print(f"O resultado da diferença é: {DIFERENCA}")
